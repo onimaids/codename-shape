6662d21292fccce3eef8e63a14afc03d038f1706
bomba-cat <bledion.morina@hotmail.com>
2025-05-17 08:30:08 +0000

    Builds for freebsd

--- build.lua
+++ build.lua
@@ -1,17 +1,17 @@
 local lfs = require("lfs")
-local zip = require("brimworks.zip")
 
 local payload_dir = "src"
 local build_dir = "build"
-local love2d_build = build_dir .. "love2d"
-
 local love2d_dir = "love2d"
-local love2d_out = "codenameshape.love"
-local payload = "payload"
+
+local love2d_bin = build_dir .. "/bin/love"
+local payload = "codenameshape.love"
 local output = "codenameshape"
 
 local buildType = "Debug"
-local platform = os.getenv("OS") or io.popen("uname"):read("*l") print(buildEnv)
+local platform = os.getenv("OS") or io.popen("uname"):read("*l")
+
+print(platform)
 
 function build_love2d()
 	os.execute("git submodule update --init --recursive")
@@ -19,19 +19,40 @@ function build_love2d()
 	os.execute("cmake --build build")
 end
 
-function generate_payload()
+function generate_payload(buildEnv)
+	if(buildEnv == "Windows_NT")
+	then
+		print("Please zip the src/ folder as " .. payload .. " for now")
+	end
+	local cmd = string.format('cd %s && zip -r ../%s .', payload_dir, payload)
+	os.execute(cmd)
 end
 
-function concat()
+function concat(buildEnv)
+	if(buildEnv == "FreeBSD")
+	then
+		love2d_bin = io.popen("which love"):read("*l") print(love2d_bin)
+	end
+
+	local in1 = assert(io.open(love2d_bin, "rb"))
+	local in2 = assert(io.open(payload, "rb"))
+	local out = assert(io.open(output, "wb"))
+
+	out:write(in1:read("*a"))
+	out:write(in2:read("*a"))
+
+	in1:close()
+	in2:close()
+	out:close()
 end
 
 function build(buildEnv)
-	--if(buildEnv != "Windows_NT")
-	--then
-	build_love2d()
-	generate_payload()
-	concat()
-	--end
+	generate_payload(buildEnv)
+	if(buildEnv ~= "FreeBSD") -- FreeBSD requires some fixing for now
+	then
+		build_love2d()
+	end
+	concat(buildEnv)
 end
 
 function main()
